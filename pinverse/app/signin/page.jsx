7813fc755c258804c167cfce3f4be68a660e9c61
aqibfaisal006 <aqibfaisal9@gmail.com>
2026-04-20 23:07:08 +0500

    working on signin page

--- pinverse/app/signin/page.jsx
+++ pinverse/app/signin/page.jsx
@@ -28,6 +28,41 @@ const page = () => {
         setLoading(false);
         return;
     }
-
 }
+
+const res = await signIn("credentials",{
+        redirect:false,
+        username,
+        password
+    }) ;
+    setLoading(false);
+    if(res?.ok){
+        router.push("/");
+    } else {
+        toast.error("Invalid credentials");
+    }
+    }
+
+      return (
+        <>
+            <div className='min-h-screen flex justify-center items-center bg-gray-100 fix top-0 left-0 w-full'>
+                <div className='bg-white p-8 rounded-lg shadow-lg max-w-sm w-full'>
+                    <div className='flex justify-center mb-4'>
+                        <img
+                            src="logoo.jpg" alt="Pinterest Svg" height={150} width={150} className="w-42 h-32 mx-auto"
+                        />
+                    </div>
+
+                    <h2 className='text-center text-xl font-semibold mb-1'>
+                        Login to see more
+                    </h2>
+                    <p className='text-center text-grey-500 mb-6'>
+                        Access PinBoard best idea with a free account
+                    </p>
+                    </div>
+                </div>
+            </>
+        )
+                   
+
 }
