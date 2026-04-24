7343222ac8fb6792b5b348a4e3a09a016c341f53
Haad majeed <haadmajeedch27@gmail.com>
2026-04-24 11:37:45 +0500

    MongoDB Setup & Connection with MongoDB Compass

---
+++ pinverse/libs/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
+
+export default connectToDB
